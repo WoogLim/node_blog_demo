aceefd72a22a85537462f2d6f40ffc0378e7e901
WoogLim <elegy74@gmail.com>
2023-03-01 18:40:15 +0900

    http log view

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ connect();
 app.use(express.json())
 
 app.use("/community", communityRouter)
-app.use(morgan(`dev`));
+app.use(morgan('dev', { stream: httpLogStream }));
 
 app.get('/', (req, res) => {
   res.send('api만 제공하는 서버입니다. 현재 경로에 /community 를 붙여 api를 사용해주세요.');
