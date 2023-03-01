55680fe050b97e8fb3383b8f98137e50ded5f33b
WoogLim <elegy74@gmail.com>
2023-03-01 18:27:51 +0900

    update project2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const port = 3001;
+const port = 3000;
 
 const communityRouter = require("./routes/community")
 const morgan = require("morgan")
